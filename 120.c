ea164d19b04d8662af9e7a9734d10101b3ae377b
Juan Medina <121105074+JuanManuelMedinaSanchez@users.noreply.github.com>
2023-09-15 10:00:28 -0500

    Update 120.c

--- 120.c
+++ 120.c
@@ -1,17 +1,14 @@
 #include <stdio.h>
 
-// Macro for setting multiple bits
-#define SET_BITS(number, mask) ((number) |= (mask))
-
 int main() {
-    unsigned int flags = 0; // Initialize flags variable with all bits set to 0
+    unsigned int flags = 0b00000000; // Initialize flags variable with all bits set to 0
 
-    // Set bits at positions 2 and 4 (0-based index)
-    unsigned int bitMask = (1 << 2) | (1 << 4);
-    SET_BITS(flags, bitMask);
+    // Set bits at positions 2 and 4 (0-based index) using binary representations
+    unsigned int bitMask = 0b00000100 | 0b00010000; // Binary: 00000100 | 00010000
+    flags |= bitMask;
 
-    // Output the updated value of 'flags'
-    printf("Updated flags: %u\n", flags);
+    // Output the updated value of 'flags' in binary format
+    printf("Updated flags: %08b\n", flags);
 
     return 0;
 }
