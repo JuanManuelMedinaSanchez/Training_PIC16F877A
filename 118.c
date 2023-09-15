78c872c92b9c2f161a591a4f37fe4f63fbd0cd11
Juan Medina <121105074+JuanManuelMedinaSanchez@users.noreply.github.com>
2023-09-15 09:40:52 -0500

    Update 118.c ->without Macro definitions

--- 118.c
+++ 118.c
@@ -1,28 +1,23 @@
 #include <stdio.h>
 
-// Macros for bit manipulation
-#define SET_BIT(number, position) ((number) |= (1 << (position)))
-#define CLEAR_BIT(number, position) ((number) &= ~(1 << (position)))
-#define CHECK_BIT(number, position) (((number) & (1 << (position))) != 0)
-
 int main() {
     unsigned int flags = 0; // Initialize flags variable with all bits set to 0
 
     // Set bit at position 2 (0-based index)
-    SET_BIT(flags, 2);
+    flags |= (1 << 2);
 
     // Check if bit at position 2 is set
-    if (CHECK_BIT(flags, 2)) {
+    if ((flags & (1 << 2)) != 0) {
         printf("Bit at position 2 is set.\n");
     } else {
         printf("Bit at position 2 is not set.\n");
     }
 
     // Clear bit at position 2
-    CLEAR_BIT(flags, 2);
+    flags &= ~(1 << 2);
 
     // Check again if bit at position 2 is set
-    if (CHECK_BIT(flags, 2)) {
+    if ((flags & (1 << 2)) != 0) {
         printf("Bit at position 2 is set.\n");
     } else {
         printf("Bit at position 2 is not set.\n");
@@ -30,3 +25,4 @@ int main() {
 
     return 0;
 }
+
