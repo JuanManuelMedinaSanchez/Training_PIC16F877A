ad94e8b9936b1d5c7f38b56e5fb700136cccbf29
Juan Medina <121105074+JuanManuelMedinaSanchez@users.noreply.github.com>
2023-09-15 09:52:06 -0500

    Update 119.c

--- 119.c
+++ 119.c
@@ -1,16 +1,25 @@
 #include <stdio.h>
 
-// Macro for setting a single bit
-#define SET_BIT(number, position) ((number) |= (1 << (position)))
-
 int main() {
-    unsigned int flags = 0; // Initialize flags variable with all bits set to 0
+    unsigned int flags = 0b00000000; // Initialize flags variable with all bits set to 0
 
-    // Set bit at position 2 (0-based index)
-    SET_BIT(flags, 2);
+    // Set bit at position 2 (0-based index) to 1
+    flags |= 0b00000100;  // Binary: 00000100
 
-    // Output the updated value of 'flags'
-    printf("Updated flags: %u\n", flags);
+    // Output the updated value of 'flags' in binary format
+    printf("Updated flags (after setting bit 2): %08b\n", flags);
+
+    // Clear the same bit at position 2 (set it to 0)
+    flags &= 0b11111011;  // Binary: 11111011
+
+    // Output the updated value of 'flags' in binary format
+    printf("Updated flags (after clearing bit 2): %08b\n", flags);
+
+    // Toggle the same bit at position 2
+    flags ^= 0b00000100;  // Binary: 00000100
+
+    // Output the updated value of 'flags' in binary format
+    printf("Updated flags (after toggling bit 2): %08b\n", flags);
 
     return 0;
 }
