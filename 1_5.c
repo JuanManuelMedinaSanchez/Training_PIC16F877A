26536f5ade3e6b870a266c0bc93b78035c14b34a
Juan Medina <121105074+JuanManuelMedinaSanchez@users.noreply.github.com>
2023-09-19 10:34:24 -0500

    Update 1_5.c

--- 1_5.c
+++ 1_5.c
@@ -1,16 +1,11 @@
 #include <stdio.h>
 
-// Function declarations
-int add(int a, int b);
 
 int main() {
     // Variable declarations and initialization
     int operand1 = 10;
     int operand2 = 5;
-    int result;
-
-    // Calculate the sum using the 'add' function
-    result = add(operand1, operand2);
+    int result = operand1 + operand2;
 
     // Output the result
     printf("The sum of %d and %d is: %d\n", operand1, operand2, result);
@@ -18,9 +13,3 @@ int main() {
     return 0; // Return 0 to indicate successful execution
 }
 
-// Function definition for addition
-int add(int a, int b) {
-    // Perform addition
-    int sum = a + b;
-    return sum;
-}
