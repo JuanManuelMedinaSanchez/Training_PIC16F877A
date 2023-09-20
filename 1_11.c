a071b278a64de806851e0a1dc3401f51f9efb7d0
Juan Medina <121105074+JuanManuelMedinaSanchez@users.noreply.github.com>
2023-09-19 22:54:54 -0500

    Update 1_11.c

--- 1_11.c
+++ 1_11.c
@@ -1,1 +1,15 @@
+#include <stdio.h>
+
+int main() {
+    unsigned int value = 0b1100;  // Binary: 1100
+
+    printf("Original Value: %08b\n", value);
+
+    // Toggle the bits using ^=
+    value ^= 0xFFFF;  // Invert all bits to toggle
+
+    printf("Toggled Value: %08b\n", value);
+
+    return 0;
+}
 
