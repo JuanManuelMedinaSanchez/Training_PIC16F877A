f96632f30969d7d5fe700841481129d14177fcc2
Juan Medina <121105074+JuanManuelMedinaSanchez@users.noreply.github.com>
2023-09-21 14:57:14 -0500

    Update 1_7-1.c

--- 1_7-1.c
+++ 1_7-1.c
@@ -3,6 +3,12 @@
 /* _________________________________________________________________________________________
 uint8_t bit1= 0b0000;
 uint8_t bit2= 0b0010;
+---> uint8_t es un tipo de dato entero sin signo (unsigned integer) que se utiliza para representar numeros
+enteros de 8 bits (1 byte) en C y C++. El nombre "uint8_t" proviene de la especificacion de tipos enteros 
+definidos en C99 y posteriores, uint8_t garantiza la ocupancia exactamente 8 bits, sirve cuando se necesita 
+un tamaño de datos específico y conocido, como en la programacion de microcontroladores o en la manipulación 
+de datos binarios, manipulación de bits y conservacion de memoria. uint8_t es siempre sin signo,
+lo que significa que solo puede representar números no negativos, desde 0 hasta 255 en el caso de 8 bits.
 */ _________________________________________________________________________________________
 
 #include<stdio.h>
