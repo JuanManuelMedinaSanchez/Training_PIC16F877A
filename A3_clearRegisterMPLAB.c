27c1e657681120a44a1da8922ff875931ef92c4c
Juan Medina <121105074+JuanManuelMedinaSanchez@users.noreply.github.com>
2023-09-21 15:09:16 -0500

    Update A3_clearRegisterMPLAB.c

--- A3_clearRegisterMPLAB.c
+++ A3_clearRegisterMPLAB.c
@@ -1,1 +1,23 @@
-C
+#include <xc.h>
+#include <stdint.h>
+
+#define _XTAL_FREQ 800000
+
+void main() {
+    TRISB = 0x00; // Set all pins of PORTB as outputs
+    uint8_t flags = 0b00000001; // Initialize flags with the 0th bit set
+
+    while (1) {
+        // Clear the 0th bit (turn off the LED)
+        flags &= 0b11111110;
+        PORTB = flags; // Update PORTB to reflect the changed flags
+
+        __delay_ms(500); // Delay for 500 milliseconds
+
+        // Set the 0th bit (turn on the LED)
+        flags |= 0b00000001;
+        PORTB = flags; // Update PORTB to reflect the changed flags
+
+        __delay_ms(500); // Delay for 500 milliseconds
+    }
+}
