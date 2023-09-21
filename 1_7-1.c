16111d7852ad810cd3737c5734a54c9db2ac6f98
Juan Medina <121105074+JuanManuelMedinaSanchez@users.noreply.github.com>
2023-09-21 14:37:48 -0500

    Update 1_7-1.c

--- 1_7-1.c
+++ 1_7-1.c
@@ -6,10 +6,10 @@
 
 //Operaciones con numeros binarios, inclusion de caracteres tipo uint8_t y relacionados
 int main() {
-	unsigned char bit1= 0b0001;
-	unsigned char bit2= 0b0001;
-	unsigned boolOp= 0;
-	boolOp = bit1 && bit2;
-	printf("resultado %08b", boolOp);
+	unsigned int bit1= 0b0000;
+	unsigned int bit2= 0b0010;
+	unsigned boolOp;
+	boolOp = bit1 & bit2;
+	printf("resultado %04b", boolOp);
 	return 0;
 }
