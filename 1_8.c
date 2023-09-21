208bc3767ccad2c628cc8825f7f51f356e3120d2
Juan Medina <121105074+JuanManuelMedinaSanchez@users.noreply.github.com>
2023-09-21 15:00:28 -0500

    Update 1_8.c

--- 1_8.c
+++ 1_8.c
@@ -1,12 +1,16 @@
+/*
+------------------------------------------------------------------------------------------
+Una bandera of flag puede ser un bit o un conjunto de bits que se utilizan para indicar 
+un estado o una condición particular dentro de un sistema o una estructura de datos.
+------------------------------------------------------------------------------------------
+*/
 #include <stdio.h>
 
 int main() {
     unsigned int flags = 0b00000000; // Initialize flags variable with all bits set to 0
-
     // Set bits at positions 2 and 4 (0-based index) using binary representations
     unsigned int bitMask = 0b00000100 | 0b00010000; // Binary: 00000100 | 00010000
     flags |= bitMask;
-
     // Output the updated value of 'flags' in binary format
     printf("Updated flags: %08b\n", flags);
 
