b8885fc1ed8709e3555ab64e783ae1115d22df80
Juan Medina <121105074+JuanManuelMedinaSanchez@users.noreply.github.com>
2023-09-21 12:13:45 -0500

    Create 1_2_cadenas.c

---
+++ 1_2_cadenas.c
@@ -0,0 +1,13 @@
+
+#include <stdio.h>
+
+int main() {
+    char miCadena[100]; // Declarar un arreglo lo suficientemente grande para la cadena
+
+    printf("Ingresa el valor correspondiente: ");
+    scanf("%s", miCadena);
+
+    printf("Salida: %s\n", miCadena);
+
+    return 0;
+}
