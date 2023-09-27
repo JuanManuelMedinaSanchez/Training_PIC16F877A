296cef8f4eda5f110a3e48d72fd67f3efcad2908
Juan Medina <121105074+JuanManuelMedinaSanchez@users.noreply.github.com>
2023-09-27 16:40:25 -0500

    Update 2_5.c

--- 2_5.c
+++ 2_5.c
@@ -1,1 +1,23 @@
-//While function
+//Ciclo while
+#include <xc.h>
+
+#define _XTAL_FREQ 800000
+
+void main() {
+    TRISA = 0b11111110; // Configura RA0 como salida
+    PORTA = 0x00; // Inicializa el puerto A en bajo
+
+    while (1) {
+        // Enciende el LED en RA0
+        PORTA = 0b00000001;
+        
+        // Espera un momento (puedes ajustar el valor para cambiar la velocidad del parpadeo)
+        __delay_ms(500); // Espera 500 milisegundos
+
+        // Apaga el LED en RA0
+        PORTA = 0x00;
+        
+        // Espera un momento
+        __delay_ms(500); // Espera 500 milisegundos
+    }
+}
