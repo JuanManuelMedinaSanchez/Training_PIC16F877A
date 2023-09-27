2b31a457165ea72ba6423d44325cf7dc72953394
Juan Medina <121105074+JuanManuelMedinaSanchez@users.noreply.github.com>
2023-09-27 12:16:18 -0500

    Update 2_3.c

--- 2_3.c
+++ 2_3.c
@@ -1,1 +1,28 @@
-//Switch statement
+#define _XTAL_FREQ 800000
+#include <xc.h>
+
+void main() {
+    TRISA = 0x00; 
+    unsigned char trafficState = 0; // Control de estado
+    while (1) {
+        switch (trafficState) {
+            case 0: // Red
+                PORTA = 0b00000001; // ON RA0 (Semaforo en rojo)
+                __delay_ms(500); // Espera 
+                trafficState = 1; // Switch al siguiente estado y break
+                break;
+                
+            case 1: // Green
+                PORTA = 0b00000010; // ON RA1 (Amarillo)
+                __delay_ms(500); // Espera 
+                trafficState = 2; // Switch al siguiente estado y break
+                break;
+                
+            case 2: // Yellow
+                PORTA = 0b00000100; // ON RA2 (Verde)
+                __delay_ms(500); // Espera 
+                trafficState = 0; //  // Switch al siguiente estado inicial 0 y break
+                break;
+        }
+    }
+}
