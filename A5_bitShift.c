e9edf85807d2a50726ff11bd172d51de75ccf793
Juan Medina <121105074+JuanManuelMedinaSanchez@users.noreply.github.com>
2023-09-28 10:55:13 -0500

    Update A5_bitShift.c

--- A5_bitShift.c
+++ A5_bitShift.c
@@ -1,4 +1,9 @@
-  #include <xc.h>
+/* 
+ * Desarrollo / Autoria, Juan Manuel Medina, mail to juansth@gmail.com
+ * Processor: PIC16F877A
+ * Compiler:  MPLAB XC8
+ */
+#include <xc.h>
 #include <stdint.h>
 
 #define _XTAL_FREQ 800000
