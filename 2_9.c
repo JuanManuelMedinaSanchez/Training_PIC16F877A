3cf62b71d4f227b79105cc5a8e01f56b88585965
Juan Medina <121105074+JuanManuelMedinaSanchez@users.noreply.github.com>
2023-09-28 12:11:54 -0500

    Update 2_9.c

--- 2_9.c
+++ 2_9.c
@@ -1,4 +1,5 @@
 //Continue, break
+
 #include <stdio.h>
 #include <xc.h>
 #define _XTAL_FREQ 800000
@@ -9,7 +10,7 @@ void main() {
     unsigned char mask = 0b00000001;  // Initialize a mask to set individual bits
     while (1) {
         PORTB = mask;   // Set PORTB based on the mask
-        __delay_ms(500);  // Delay for 500 milliseconds
+        __delay_ms(200);  // Delay for 500 milliseconds
         if (mask == 0b10000000) {
             // If the mask reaches the last bit, reset it and break the loop
             mask = 0b00000001;
@@ -18,10 +19,8 @@ void main() {
         mask <<= 1;  // Shift the mask to the left to set the next bit
         continue;    // Continue to the next iteration of the loop
     }
-    // The program continues here after the loop is terminated
-    // You can add your main program logic below
-    while (1) {
-        // main
-    }
+
 }
+
+
 
