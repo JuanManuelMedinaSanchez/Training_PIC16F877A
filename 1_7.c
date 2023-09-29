4b778fcb0f5c3a8ed9ceae8ea008963aa1bebc94
Juan Medina <121105074+JuanManuelMedinaSanchez@users.noreply.github.com>
2023-09-29 10:39:18 -0500

    Update 1_7.c

--- 1_7.c
+++ 1_7.c
@@ -4,14 +4,14 @@ Scripted by Juan Medina, mail to juansth@gmail.com*/
 #include <stdio.h>
 #include <stdint.h>
 
-int main() {
-    uint8_t originalValue = 0b1100; 
-    uint8_t leftShiftedValue = originalValue << 2; 
-    uint8_t rightShiftedValue = originalValue >> 2;
-
-    // Output with Explanations
-    printf("Original Value: %u (Binary: %08b)\n", originalValue, originalValue);
-    printf("Left Shifted by 2: %u (Binary:%08b)\n", leftShiftedValue, leftShiftedValue);
-    printf("Right Shifted by 1: %u (Binary: %08b)\n", rightShiftedValue, rightShiftedValue);
-    return 0;
+int
+main ()
+{
+  uint8_t bandera = 0b1100;
+  uint8_t leftShiftedValue = bandera << 2;
+  uint8_t rightShiftedValue = bandera >> 2;
+  printf ("original Binary: %08b\n", bandera);
+  printf ("Left shift Binary: %08b\n", leftShiftedValue);
+  printf ("Right Shifted Binary: %08b\n", rightShiftedValue);
+  return 0;
 }
