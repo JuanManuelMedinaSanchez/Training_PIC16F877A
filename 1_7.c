9e5f7db63713f80a94989ba04478f796217ced23
Juan Medina <121105074+JuanManuelMedinaSanchez@users.noreply.github.com>
2023-09-29 10:35:19 -0500

    Update 1_7.c

--- 1_7.c
+++ 1_7.c
@@ -1,21 +1,17 @@
 /*C script demos
-Scripted by Juan Medina, mail to juansth@gmail.com/*
+Scripted by Juan Medina, mail to juansth@gmail.com*/
+
 #include <stdio.h>
+#include <stdint.h>
 
 int main() {
-    // Bitwise Shift Operations
-    unsigned int originalValue = 0b1100; // Binary: 1100 (12 in decimal)
-
-    // Left Shift (<<) - Shifting left by 2 positions
-    unsigned int leftShiftedValue = originalValue << 2; // Binary: 110000 (48 in decimal)
-
-    // Right Shift (>>) - Shifting right by 1 position
-    unsigned int rightShiftedValue = originalValue >> 1; // Binary: 0110 (6 in decimal)
+    uint8_t originalValue = 0b1100; 
+    uint8_t leftShiftedValue = originalValue << 2; 
+    uint8_t rightShiftedValue = originalValue >> 2;
 
     // Output with Explanations
-    printf("Original Value: %u (Binary: %04b)\n", originalValue, originalValue);
-    printf("Left Shifted by 2: %u (Binary: %08b)\n", leftShiftedValue, leftShiftedValue);
-    printf("Right Shifted by 1: %u (Binary: %04b)\n", rightShiftedValue, rightShiftedValue);
-
+    printf("Original Value: %u (Binary: %08b)\n", originalValue, originalValue);
+    printf("Left Shifted by 2: %u (Binary:%08b)\n", leftShiftedValue, leftShiftedValue);
+    printf("Right Shifted by 1: %u (Binary: %08b)\n", rightShiftedValue, rightShiftedValue);
     return 0;
 }
