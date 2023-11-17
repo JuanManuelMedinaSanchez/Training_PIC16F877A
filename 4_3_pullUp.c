e412473b96141203089d9d26de0a986b52201489
Juan Medina <121105074+JuanManuelMedinaSanchez@users.noreply.github.com>
2023-11-17 08:05:01 -0500

    Update 4_3_pullUp.c

--- 4_3_pullUp.c
+++ 4_3_pullUp.c
@@ -1,30 +1,31 @@
 
-/* 
-						OPTION_REG
-BIT 7.
-pull-up -> RB0/INT (NOT_RBPU = 0) y 
-BIT 6.
-RB0/INT (INTEDG = 0) -> falling edge of the signal
-RB0/INT (INTEDG = 1) -> Rising edge of the signal
-
-*/
-
-#include <xc.h>
-#include <stdint.h>
-#define _XTAL_FREQ 8000000
-
-
-void main(void) {
-   //OPTION_REG = 0b00000000;
-   TRISD  = 0x00;
-   RD2 = 0;
-    while (1) {
-		if(RB0 == 0){
-			RD2 = 1;
-			__delay_ms(100);
-			RD2 = 0;
-		}
+/*PULL UP RESISTORS */
+#pragma config FOSC = HS        // Oscillator Selection bits (HS oscillator)
+#pragma config WDTE = OFF       // Watchdog Timer Enable bit (WDT disabled)
+#pragma config PWRTE = OFF      // Power-up Timer Enable bit (PWRT disabled)
+#pragma config BOREN = OFF     // Brown-out Reset Enable bit (BOR enabled)
+#pragma config LVP = OFF        // Low-Voltage (Single-Supply) In-Circuit Serial Programming Enable bit (RB3 is digital I/O, HV on MCLR must be used for programming)
+#pragma config CPD = OFF        // Data EEPROM Memory Code Protection bit (Data EEPROM code protection off)
+#pragma config WRT = OFF        // Flash Program Memory Write Enable bits (Write protection off; all program memory may be written to by EECON control)
+#pragma config CP = OFF         // Flash Program Memory Code Protection bit (Code protection off)
+ 
+void pullUp(void){
+    PORTB=0x00;
+    PORTD=0x00;
+    TRISB=0xFF;
+    /*Turn on Weak Pull Up*/
+    OPTION_REG = 0b10000000;
+    /*Port C output*/
+    TRISD=0x00;
+    while(PORTB){
+       PORTD = 1;
     }
 }
+
+void main(void){
+   
+   pullUp();
+   
+   }
 
 
