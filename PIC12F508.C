20ecc7cba96eea3d4132e4f1e4f69e4d52055672
Juan Medina <121105074+JuanManuelMedinaSanchez@users.noreply.github.com>
2024-01-10 22:57:09 -0500

    Update PIC12F508.C

--- PIC12F508.C
+++ PIC12F508.C
@@ -1,25 +1,23 @@
-   #pragma config OSC = IntRC 
-   #pragma config WDT = OFF 
-   #pragma config CP = OFF 
-   #pragma config MCLRE = OFF
-   #include <xc.h>
-   #define _XTAL_FREQ  4000000
+#pragma config OSC = IntRC 
+#pragma config WDT = OFF 
+#pragma config CP = OFF 
+#pragma config MCLRE = OFF
+#include <xc.h>
+#define _XTAL_FREQ  4000000
 
-   void blinkLED() {
-	 GP0 = 1;     // Turn on the LED
-       __delay_ms(500);   // Wait for a while (adjust as needed)
-	 GP0 = 0;         // Turn off the LED
-       __delay_ms(500);   // Wait for a while  
-   }
-
-   void main(void) {
-TRISGPIO   = 0b11000000;
- GPIO = 0x00000000;
-
-       while (1) {
-	   blinkLED();
-       }
-   }
-
-
+ void blinkLED (){
+GP0 = 1;			
+  __delay_ms (500);	
+  GP0 = 0;			
+  __delay_ms (500);		
+} 
+ 
+void main (void){
+TRISGPIO = 0 b11000000;
+while (1)
+    {
+        blinkLED ();
+    }
+return;
+}
 
