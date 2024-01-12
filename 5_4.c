e6212fdcb9d2628a342b3cb526d2e39cf056e328
Juan Medina <121105074+JuanManuelMedinaSanchez@users.noreply.github.com>
2024-01-12 18:16:20 -0500

    Update and rename 5_4 to 5_4.c

--- 5_4.c
+++ 5_4.c
@@ -1,31 +1,32 @@
 #include <xc.h>
 #include <stdint.h>
 
+#define LED_PIN RB1     // Asigna el pin del LED al puerto B, pin 1
+#define _XTAL_FREQ 4000000 //
 // Definición de la estructura para representar la configuración de un pin del MCU
-typedef struct {
+struct PinConfig {
     uint8_t pinNumber;  // Número del pin del MCU
     uint8_t state;      // Estado del pin (encendido: 1, apagado: 0)
-} PinConfig;
+};
 
 // Configuración específica para el PIC16F877A y MPLAB X IDE con XC8
-#define LED_PIN  RB0     // Asigna el pin del LED al puerto B, pin 0
 
 // Función para inicializar la configuración de un pin
-void initPin(PinConfig *pin) {
+void initPin(struct PinConfig *pin) {
     // Configuración específica para el PIC16F877A
     TRISB &= ~(1 << pin->pinNumber); // Configura el pin como salida
     pin->state = 0;
 }
 
 // Función para cambiar el estado del pin a encendido
-void turnOnPin(PinConfig *pin) {
+void turnOnPin(struct PinConfig *pin) {
     // Configuración específica para el PIC16F877A
     PORTB |= (1 << pin->pinNumber); // Establece el pin en alto (encendido)
     pin->state = 1;
 }
 
 // Función para cambiar el estado del pin a apagado
-void turnOffPin(PinConfig *pin) {
+void turnOffPin(struct PinConfig *pin) {
     // Configuración específica para el PIC16F877A
     PORTB &= ~(1 << pin->pinNumber); // Establece el pin en bajo (apagado)
     pin->state = 0;
@@ -33,8 +34,8 @@ void turnOffPin(PinConfig *pin) {
 
 int main(void) {
     // Configuración del pin para un LED
-    PinConfig myLEDPin;
-    myLEDPin.pinNumber = 0; // RB0 en el PIC16F877A
+    struct PinConfig myLEDPin;
+    myLEDPin.pinNumber = 1; // RB1 en el PIC16F877A
 
     // Inicializa la configuración del pin
     initPin(&myLEDPin);
@@ -42,10 +43,10 @@ int main(void) {
     while (1) {
         // Tu código principal aquí
         turnOnPin(&myLEDPin);   // Enciende el LED
-        __delay_ms(1000); // Pausa de 1000 ms (1 segundo)
+        __delay_ms(500); // Pausa de 500 ms
         turnOffPin(&myLEDPin);  // Apaga el LED
-        __delay_ms(1000); // Pausa de 1000 ms (1 segundo)
+        __delay_ms(500); // Pausa de 500 ms
     }
 
-    return;
+    return 0;
 }
